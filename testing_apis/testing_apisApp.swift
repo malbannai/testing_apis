f36fef5bf0e01fb39ecdfe5c66065929487c5b7f
malbannai <moudhi.albannai@gmail.com>
2022-12-10 10:28:43 +0300

    Initial Commit

---
+++ testing_apis/testing_apisApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_apisApp.swift
+//  testing_apis
+//
+//  Created by Moudhi on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct testing_apisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
